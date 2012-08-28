513d5dba726b5cbd4c2c8873d25ef979db04aa00
Josh Blum <josh@joshknows.com>
2012-08-27 21:09:24 -0700

    runtime: other changes to work with minimal core changes

---
+++ include/gnuradio/gr_vmcircbuf.h
@@ -0,0 +1,10 @@
+
+//totally bogus so I dont have to change gnuradio-core/src/tests/CMakeLists.txt
+
+struct gr_vmcircbuf_sysconfig
+{
+    static bool test_all_factories(int)
+    {
+        return true;
+    }
+};
