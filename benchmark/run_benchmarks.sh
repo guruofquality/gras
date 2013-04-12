332d4d256b706360d3df81d6c9f5c9fc7b876aed
Josh Blum <josh@joshknows.com>
2013-04-12 01:06:04 -0700

    gras: added suffix for benchmark results

--- benchmark/run_benchmarks.sh
+++ benchmark/run_benchmarks.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+SUFFIX=$1
+
 ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
 SOURCEDIR=`dirname $ABSOLUTE_PATH`
 BUILDDIR=$(pwd)
 
-OUT_PDF="gras_benchmark.pdf"
-OUT_PNG="gras_benchmark.png"
+OUT_PDF="gras_benchmark_${SUFFIX}.pdf"
+OUT_PNG="gras_benchmark_${SUFFIX}.png"
 
 echo "source directory ${SOURCEDIR}"
 echo "build directory ${BUILDDIR}"
 
 python -B $SOURCEDIR/run_benchmarks.py ${OUT_PDF}
-convert ${OUT_PDF} /tmp/tmp_gras_bm.png
-convert /tmp/tmp_gras_bm*.png -append ${OUT_PNG}
-rm /tmp/tmp_gras_bm*.png
+convert ${OUT_PDF} /tmp/tmp_gras_bm_${SUFFIX}.png
+convert /tmp/tmp_gras_bm_${SUFFIX}*.png -append ${OUT_PNG}
+rm /tmp/tmp_gras_bm_${SUFFIX}*.png
