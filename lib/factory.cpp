147ae8c83b6237afaafe076feeedf50cc0d374fa
Josh Blum <josh@joshknows.com>
2013-07-27 20:41:50 -0700

    gras: just warn if the factory is already registered

--- lib/factory.cpp
+++ lib/factory.cpp
@@ -5,6 +5,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/thread/mutex.hpp>
 #include <stdexcept>
+#include <iostream>
 #include <map>
 
 using namespace gras;
@@ -40,7 +41,7 @@ void Factory::_register_make(const std::string &name, void *entry)
     boost::mutex::scoped_lock l(mutex);
     if (get_factory_registry().count(name) != 0)
     {
-        throw std::invalid_argument("Factory - function already registered for name: " + name);
+        std::cerr << "Warning: Factory - function already registered for name: " + name << std::endl;
     }
     get_factory_registry()[name].reset(reinterpret_cast<FactoryRegistryEntry *>(entry));
 }
